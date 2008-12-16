ee6a99f1f2443522002e59deebfc3e8e4cc54bb4
lopnor <lopnor@d0d07461-0603-4401-acd4-de1884942a52>
2008-12-16 14:59:25 +0000

    first import
    git-svn-id: http://svn.coderepos.org/share/lang/perl/Net-Google-Spreadsheets/trunk@26898 d0d07461-0603-4401-acd4-de1884942a52

---
+++ lib/Net/Google/Spreadsheets.pm
@@ -0,0 +1,33 @@
+package Net::Google::Spreadsheets;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Net::Google::Spreadsheets -
+
+=head1 SYNOPSIS
+
+  use Net::Google::Spreadsheets;
+
+=head1 DESCRIPTION
+
+Net::Google::Spreadsheets is
+
+=head1 AUTHOR
+
+Nobuo Danjou E<lt>nobuo.danjou@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
